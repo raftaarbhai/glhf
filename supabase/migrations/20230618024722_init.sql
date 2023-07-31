962b1d34a3f41b0f90f78c3a32a8516e4c5bcd47
raftaar bhai <bhai@gmail.com>
2023-07-31 21:06:11 +0200

    feat: update init sql

--- supabase/migrations/20230618024722_init.sql
+++ supabase/migrations/20230618024722_init.sql
@@ -1,18 +1,27 @@
-create table if not exists todos (
-  id uuid default gen_random_uuid() primary key,
-  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
-  title text,
-  is_complete boolean default false,
-  user_id uuid references auth.users default auth.uid()
-);
+-- Create a table called "data" with columns "prompt", "response-1", "response-2", "category", "original_response", and "external_id"
+create table if not exists
+  data (
+    prompt text,
+    "response_one" text,
+    "response_two" text,
+    "response_three" text,
+    "response_four" text,
+    category text,
+    original_response text,
+    external_id text,
+    id integer primary key generated always as identity
+  );
+
+alter table data
+add column user_id uuid references auth.users (id) on delete cascade;
 
 -- Set up Row Level Security (RLS)
 -- See https://supabase.com/docs/guides/auth/row-level-security for more details.
-alter table todos
+alter table data
   enable row level security;
 
-create policy "Authenticated users can select todos" on todos
+create policy "Authenticated users can select their own data" on data
   for select to authenticated using (true);
 
-create policy "Authenticated users can insert their own todos" on todos
+create policy "Authenticated users can insert their own data" on data
   for insert to authenticated with check (auth.uid() = user_id);
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,12 @@
-insert into todos(title)
-values
-  ('Create Supabase project'),
-  ('Create Next.js app from Supabase Starter template'),
-  ('Keep building cool stuff!');
+
+-- Create a table called "data" with columns "prompt", "response-1", "response-2", "category", "original_response", and "external_id"
+create table
+  data (
+    prompt text,
+    "response-1" text,
+    "response-2" text,
+    category text,
+    original_response text,
+    external_id text,
+    id integer primary key generated always as identity
+  );
