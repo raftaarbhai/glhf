969dad3f67c8f052bd616544035175dbf9ca8574
raftaar bhai <bhai@gmail.com>
2023-08-20 18:53:47 +0200

    chore: fix get route

--- app/api/predictions/[id]/route.tsx
+++ app/api/predictions/[id]/route.tsx
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-export async function POST(req) {
-  const reqJSON = await req.json();
-  console.log(reqJSON);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+
+  console.log(params.id)
   const response = await fetch(
-    "https://api.replicate.com/v1/predictions/" + reqJSON.query.id,
+    "https://api.replicate.com/v1/predictions/" + params.id,
     {
       headers: {
         Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
